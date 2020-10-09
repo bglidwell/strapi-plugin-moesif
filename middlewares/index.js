1e77c900646a77d8e3fc47470e92111a70e2563c
bglidwell <sintex+github@gmail.com>
2020-10-08 21:38:00 -0500

    move strapi config reference

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,15 +1,15 @@
 const moesif = require('moesif-nodejs');
 
-const moesifMiddleware = moesif({
-  applicationId: strapi.config.middleware.settings.moesif.appid,
-
-  // Optional hook to link API calls to users
-  identifyUser: function (req, res) {
-    return req.user ? req.user.id : undefined;
-  },
-});
-
 module.exports = strapi => {
+  const moesifMiddleware = moesif({
+    applicationId: strapi.config.middleware.settings.moesif.appid,
+  
+    // Optional hook to link API calls to users
+    identifyUser: function (req, res) {
+      return req.user ? req.user.id : undefined;
+    },
+  });
+
   return {
     // can also be async
     initialize() {
