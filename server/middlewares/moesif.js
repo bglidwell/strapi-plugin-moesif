9f5b78c051b86e1bd0b32f82ca2ea5c4a2c747f4
Bobby Glidwell <sintex+github@gmail.com>
2021-12-09 08:35:14 -0600

    gracefully error on missing applicationId

--- server/middlewares/moesif.js
+++ server/middlewares/moesif.js
@@ -4,6 +4,13 @@ const moesif = require("moesif-nodejs");
 
 module.exports = (options, { strapi }) => {
   const moesifConfig = strapi.plugin("strapi-plugin-moesif").config("moesif");
+  if (!moesifConfig.applicationId || moesifConfig.applicationId === "") {
+    strapi.log.error(
+      "Unable to load moesif plugin: missing applicationId ENV variable"
+    );
+    return;
+  }
+  
   const moesifMiddleware = moesif(moesifConfig);
 
   return moesifMiddleware;
