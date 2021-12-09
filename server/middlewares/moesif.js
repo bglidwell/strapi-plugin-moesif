d407c7d25e2c4330a727ce32acb4770db3e9cf8a
Bobby Glidwell <sintex+github@gmail.com>
2021-12-09 08:44:00 -0600

    clearer error message for missing env variable

--- server/middlewares/moesif.js
+++ server/middlewares/moesif.js
@@ -6,11 +6,11 @@ module.exports = (options, { strapi }) => {
   const moesifConfig = strapi.plugin("strapi-plugin-moesif").config("moesif");
   if (!moesifConfig.applicationId || moesifConfig.applicationId === "") {
     strapi.log.error(
-      "Unable to load moesif plugin: missing applicationId ENV variable"
+      "Unable to load moesif plugin: missing MOESIF_APPLICATION_ID environment variable"
     );
     return;
   }
-  
+
   const moesifMiddleware = moesif(moesifConfig);
 
   return moesifMiddleware;
